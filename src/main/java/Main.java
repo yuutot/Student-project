734c6928d41051e6bfe639d4cebd0c78e704fb3a
yuutot <bsyuuto@gmail.com>
2016-10-13 01:05:10 +0300

    1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,6 @@
-import com.itea.dao.EntityDao;
-import com.itea.dao.impl.MarkDaoImpl;
-import com.itea.entity.Lesson;
-import com.itea.entity.Mark;
-import com.itea.entity.Student;
-import com.itea.entity.Teacher;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.sql.Date;
-
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext ctx =new ClassPathXmlApplicationContext("classpath:application-context.xml");
-        EntityDao<Mark> dao = ctx.getBean(MarkDaoImpl.class);
+
     }
 }
