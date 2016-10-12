08c8ee2e0a1b1c8edfa6e8ff46bf0856fd78fd7a
yuutot <bsyuuto@gmail.com>
2016-10-13 01:04:10 +0300

    1.0

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.sql.Date;
-
 public class JUnitTest {
     @Test
     public void isSaveAndLoad(){
