d7e790fb5f13f30a18fb8b7e6d8c656266c34818
yuutot <bsyuuto@gmail.com>
2016-11-10 21:20:52 +0200

    test

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
  * Created by yuuto on 11/10/16.
  */
 
-public class Tets {
+public class MyTest {
     @Test
     public void test(){
         Assert.assertEquals(true,true);
